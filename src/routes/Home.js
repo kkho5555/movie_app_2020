d5c49ebd1cf07b1b73285266ef9282a062d26041
kkho5555 <62885208+kkho5555@users.noreply.github.com>
2020-05-21 03:19:38 +0900

    finish

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Movie from '../components/Movie';
 import './Home.css';
+
 class Home extends React.Component {
   state = {
     isLoading: true,
@@ -47,4 +48,5 @@ class Home extends React.Component {
     );
   }
 }
-export default App;
+
+export default Home;
